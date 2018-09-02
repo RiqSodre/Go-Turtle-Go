2e2cae2d087b93ffa60c195001f17376f2f8f8e4
Ricardo Sodre <ricardo.sodre@facens.br>
2018-09-02 16:36:30 -0300

    Smooth Camera

---
+++ Prj_GoTurtleGo/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+
+    public Transform turtle;
+    public Vector3 offset;
+
+    public float smoothSpeed = 0.125f;
+
+    void FixedUpdate()
+    {
+        Vector3 diseredPosition = turtle.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, diseredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+    
+}
---
+++ Prj_GoTurtleGo/Assets/Scripts/Turtle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour {
+
+    public float speed = 20f;
+	
+	void Update () {
+        transform.position += new Vector3(Input.acceleration.x, Input.acceleration.y, 0) * Time.deltaTime * speed;
+	}
+}

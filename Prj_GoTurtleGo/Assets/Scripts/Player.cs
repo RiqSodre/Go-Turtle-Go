704caa63d679e69db632090918239ab404d191d7
Ricardo Sodre <ricardo.sodre@facens.br>
2018-09-09 19:59:21 -0300

    Cinemachine, HUD

--- Prj_GoTurtleGo/Assets/Scripts/Player.cs
+++ Prj_GoTurtleGo/Assets/Scripts/Player.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
     //Váriaveis para controlar a velocidade.
-    public float speedY = 30f;
-    public float currentSpeed = 0f;
-    public float speedX = 10f;
+    public float speedY = 30f;              //Velocidade vertical.
+    public float currentSpeed = 0.0f;       //Velocidade atual.
     
 	void Update () {
         transform.position += new Vector3(currentSpeed, Input.acceleration.x * speedY, 0) * Time.deltaTime;
 
-        if(currentSpeed < 5)
-            currentSpeed += speedX;
+        if(currentSpeed < 7)
+            currentSpeed += 1;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Inimigo"))
+        {
+            currentSpeed = -2;
+        }
     }
 }

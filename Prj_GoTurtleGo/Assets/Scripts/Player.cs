fa499b4fc9697dd4b3c0597782750cdb0aba9e36
Ricardo Sodre <ricardo.sodre@facens.br>
2018-09-12 13:16:45 -0300

    Cinemachine, Rede

--- Prj_GoTurtleGo/Assets/Scripts/Player.cs
+++ Prj_GoTurtleGo/Assets/Scripts/Player.cs
@@ -5,22 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
-    //Váriaveis para controlar a velocidade.
-    public float speedY = 30f;              //Velocidade vertical.
-    public float currentSpeed = 0.0f;       //Velocidade atual.
-    
-	void Update () {
+    #region Váriaveis
+    public int energia;
+    public int folego;
+    public float velocidade;
+
+    public float speedY = 30f; 
+    public float currentSpeed = 0.0f;
+    public float negativeSpeed = -8f;
+    #endregion
+
+    void Update () {
         transform.position += new Vector3(currentSpeed, Input.acceleration.x * speedY, 0) * Time.deltaTime;
 
         if(currentSpeed < 7)
-            currentSpeed += 1;
+            currentSpeed += 0.25f;
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Inimigo"))
         {
-            currentSpeed = -2;
+            currentSpeed = negativeSpeed;
         }
     }
 }
---
+++ Prj_GoTurtleGo/Assets/Scripts/Rede.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rede : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public GameObject player;
+    public float speed = 3;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate () {
+        rb.AddForce(transform.right * speed);
+        rb.velocity = rb.velocity.normalized * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (player)
+        {
+            print("Bateu");
+            Destroy(gameObject);
+        }
+    }
+}
